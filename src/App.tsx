13fa5b758e26e0aeff7997ec6df27fba3dde2bfa
Mariano Melendez <marianomelend@gmail.com>
2023-05-08 14:19:43 -0300

    1st commit renderedForm

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import {Box} from '@mui/material'
+import Form from './components/form'
+function App() {
+
+  return (
+    <Box>
+      <Form/>
+    </Box>
+  )
+}
+
+export default App
